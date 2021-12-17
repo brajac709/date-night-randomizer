58e7bb946491e2742c08c6aae1d27401b061bde5
brajac709 <bij4@cornell.edu>
2021-12-16 21:22:04 -0500

    add git aliases

---
+++ .gitpod/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# TODO this should probably live in another repo so we can re-use it
+# but for now I only plan to use Gitpod for this repo
+
+# Set-up Git Aliases
+git config alias.co = checkout
+git config alias.tree = log --graph --oneline
