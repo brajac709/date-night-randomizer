2aebb692c60c2c39486c94f93200516c75715083
Brady Jacobs <bij4@cornell.edu>
2021-12-19 20:04:15 +0000

    Implement settingsProvider get and set methods

--- src/DateNightRandomizerConsole/App/settingsProvider.ts
+++ src/DateNightRandomizerConsole/App/settingsProvider.ts
@@ -1,25 +1,59 @@
 import { Settings } from './settings';
+import { DateNightData } from './dateNightData';
 import * as fs from 'fs';
-import { resolve } from 'url';
+// TODO we can probably use builtin promise library, 
+// I just need to figure out how to include it in the project
+//import * as fsPromises from 'fs/promises';
+// import { resolve } from 'url';
 
 export class SettingsProvider {
+    fileName : string = 'test.json'
+
     async get() : Promise<Settings> {
-        // TODO retrieve settings
         var promise = new Promise<Settings>((resolve, reject) => {
-            fs.readFile('test.json', 'utf8',(err,data) => {
-                if (err) reject(); // TODO throw err????
+            fs.readFile(this.fileName, 'utf8', (err, data) => {
+                if (err)  {
+                    if(err.code == 'ENOENT')
+                    {
+                        const settings = this.createDefaultSettings();
+                        this.set(settings).then(() => resolve(settings), (reason) => reject(reason))
+                    }
+                    else {
+                        reject(err); // TODO throw err????
+                    }
+                }
 
                 const settings = this.parseSettings(data);
                 resolve(settings);  // I think??? 
             });
         });
+
         return promise;
+    }
+
+    async set(settings : Settings) : Promise<void> {
+        const promise = new Promise<void>((resolve, reject) => {
+            const settingString = JSON.stringify(settings);
+            fs.writeFile(this.fileName, settingString, 'utf8', (err) => {
+                if (err)  {
+                    reject(err);
+                }
+
+                resolve();
+            });
+        });
+        return promise;
+    }
+
+    // TODO maybe this should exist statically in settings.ts???
+    createDefaultSettings() : Settings {
+        return { 
+            events: []
+        };
     }
 
     // TODO this may come from a separate Serializer/Deserializer class 
     private parseSettings(data : string) : Settings {
-        return null;
+        return JSON.parse(data);
     }
-
-
 }
