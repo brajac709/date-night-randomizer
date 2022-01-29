5e2e6c0ea46795e38b801900b41b476878d1cb18
Brady Jacobs <bij4@cornell.edu>
2022-01-29 17:12:47 +0000

    Update WebApp with endpoints

--- src/DateNightRandomizerConsole/App/webApp.ts
+++ src/DateNightRandomizerConsole/App/webApp.ts
@@ -1,38 +1,13 @@
 import { RandomizerApp } from "./randomizerApp";
 import { promisify } from "util";
-import  * as readline from "readline";
-import {stdin, stdout} from "process";
 import { DateNightData } from "./dateNightData";
 import { ConfigManager } from "./configManager";
 import  express from "express";
 
-const enterToContinue = "Press ENTER to continue...";
-
-interface MenuOption {
-    prompt : string,
-    fcn : () => Promise<void>
-}
-
-const buildMenuString = (options : string[], prompt? : string[]) : string => {
-    if (prompt === undefined) {
-        prompt = [ "Select Option:"]
-    }
-    const lines = [
-        "*********************", 
-        ...prompt,
-        ...options.map((val, idx) => `  ${idx+1}. ${val}`),
-        "  Else. Exit",
-        "Enter? ",
-    ];
-
-    return lines.reduce((acc, val) => acc + val + "\n", "");
-}
-
 export class WebApp {
     private readonly _randomizerApp : RandomizerApp;
-    private readonly _rl : readline.ReadLine;
-    private readonly _question : (query:string ) => Promise<string>; 
 
+    /*
     private readonly _menuOptions : MenuOption[] = [
         { prompt: "Add Event", fcn: this.addEventMenu },
         { prompt: "Pop Event", fcn: this.popEventMenu },
@@ -47,11 +22,10 @@ export class WebApp {
         { prompt: "List Events", fcn: this.listEventsMenu },
         { prompt: "Remove Event", fcn: this.removeEventMenu },
     ];
+    */
 
     constructor(randomizerApp : RandomizerApp) {
         this._randomizerApp = randomizerApp;
-        this._rl = readline.createInterface(stdin, stdout)
-        this._question = promisify(this._rl.question).bind(this._rl);
     }
 
 
@@ -59,159 +33,100 @@ export class WebApp {
         const app = express();
         const port = 3000;
 
+        const configManager = await ConfigManager.getInstance();
+
+        const debugMode = configManager.get("debugMode");
+
         app.get('/', (req,res) => {
             res.send('Hello World!!!')
+        });
+
+        app.post('/event', express.json(), async (req, res) => {
+            console.log("Adding Event");
+            const event = req.body;
+            await this._randomizerApp.addEvent(event)
+            res.sendStatus(200).send("OK");
+        });
+
+        app.get('/event', async (req, res) => {
+            console.log("Popping Event...");
+            const event = await this._randomizerApp.popEvent(); 
+
+            console.log(event);
+            res.sendStatus(200).json(event);
+        });
+
+        // TODO should this be off the popped endpoint?
+        app.post('/events/recycle', async (req, res) => {
+            console.log("Recycling events...");
+            await this._randomizerApp.recyclePoppedEvents();
+            res.sendStatus(200).send("OK");
+        });
+
+        // TODO may make this a query parameter
+        // rather than a separate endpoint????
+        // Maybe not... to handle debug mode appropriately...
+        app.get('/events/popped', async (req, res) => {
+            var events = this._randomizerApp.getPoppedEvents();
+
+            console.log("Popped Events: ");
+            console.log(JSON.stringify(events,null, 2));
+
+            res.sendStatus(200).json(events);
+        });
+
+        app.get('/events', async (req, res) => {
+            if (!debugMode) {
+                // TODO maybe use error middleware??
+                res.sendStatus(403).send("Forbidden");
+                return
+            }
+            var events = this._randomizerApp.getEvents();
+
+            console.log("Events: ");
+            console.log(JSON.stringify(events,null, 2));
+
+            res.sendStatus(200).json(events);
+        });
+
+        app.get('/events/count', async (req, res) => {
+            var num = this._randomizerApp.numberOfEvents();
+            console.log(`Number of Events Remaining: ${num}`);
+
+            res.sendStatus(200).json(num);
+        });
+
+        app.delete('/events/popped/:id(\d+)', async (req, res) => {
+            const idx = parseInt(req.params.id);
+            await this._randomizerApp.removePoppedEvent(idx);
+            res.sendStatus(200).send("OK");
+        });
+
+        app.delete('/events/:id(\d+)', async (req, res) => {
+            if (!debugMode) {
+                // TODO maybe use error middleware??
+                res.sendStatus(403).send("Forbidden");
+                return
+            }
+            const idx = parseInt(req.params.id);
+            await this._randomizerApp.removeEvent(idx);
+            res.sendStatus(200).send("OK");
+        });
+
+        app.post('/initialize', async (req, res) => {
+            if (!debugMode) {
+                // TODO maybe use error middleware??
+                res.sendStatus(403).send("Forbidden");
+                return
+            }
+            await this._randomizerApp.reinitializeSettings();
+
+            console.log("Settings Reset");
+            res.sendStatus(200).send("OK");
         });
 
         app.listen(port, () => {
             console.log(`Web App listening at http://localhost:${port}`);
         });
-    }
-
-    private async mainMenu() {
-        const configManager = await ConfigManager.getInstance();
-
-        const debugMode = configManager.get("debugMode");
-
-        const options = [...this._menuOptions];
-        if (debugMode) {
-            options.push(...this._debugOptions);
-        }
-
-        const menuString = buildMenuString(options.map(val => val.prompt));
-        const response = await this._question(menuString);
-
-
-        const idx = Number.parseInt(response);
-        if (Number.isNaN(idx)) {
-            return -1;
-        }
-
-        const entry = options[idx-1];
-        if (entry === undefined) {
-            return -1;
-        }
-
-        await entry.fcn.bind(this)();
-        return idx;
-    }
-
-    private async addEventMenu() {
-        console.log("Add Event:");
-        const name = await this._question("Event Name: ");
-        const desc = await this._question("Event Description: ");
-
-        const event : DateNightData = {
-            eventName : name,
-            eventDescription : desc
-        };
-
-        await this._randomizerApp.addEvent(event);
-        const _ = await this._question(enterToContinue);
-    }
-
-    private async popEventMenu() {
-        console.log("Popping Event...");
-        const event = await this._randomizerApp.popEvent();
-
-        console.log(event);
-        const _ = await this._question(enterToContinue);
-    }
-
-    private async recyclePoppedEventsMenu() {
-        console.log("Recycling events...");
-        await this._randomizerApp.recyclePoppedEvents();
-        const _ = await this._question(enterToContinue);
-    }
-
-    private async listPoppedEventsMenu() {
-        var events = this._randomizerApp.getPoppedEvents();
-
-        console.log("Popped Events: ");
-        console.log(JSON.stringify(events,null, 2));
-        const _ = await this._question(enterToContinue);
-    }
-
-    private async listEventsMenu() {
-        var events = this._randomizerApp.getEvents();
-
-        console.log("Events: ");
-        console.log(JSON.stringify(events,null, 2));
-        const _ = await this._question(enterToContinue);
-    }
-
-    private async numberOfEventsMenu() {
-        var num = this._randomizerApp.numberOfEvents();
-        console.log(`Number of Events Remaining: ${num}`);
-
-        const _ = await this._question(enterToContinue);
-    }
-
-    private async removePoppedEventMenu() {
-        const events = this._randomizerApp.getPoppedEvents();
-        
-        await this.removeGeneralMenu(
-            events, 
-            this._randomizerApp.removePoppedEvent.bind(this._randomizerApp));
-    }
-
-    private async removeEventMenu() {
-        const events = this._randomizerApp.getEvents();
-
-        await this.removeGeneralMenu(
-            events, 
-            this._randomizerApp.removeEvent.bind(this._randomizerApp));
-    }
-
-    private async removeGeneralMenu(
-        events : readonly DateNightData[], 
-        removeFcn : (idx :number) => Promise<void>) 
-    {
-        const prompt = [
-            "Select Event To Remove",
-            "(Negative to print description)",
-        ]
-
-        const options = events.map(val => val.eventName);
-
-        const menuString  = buildMenuString(options, prompt);
-
-        while (true) {
-            const response = await this._question(menuString);
-
-            let idx = Number.parseInt(response);
-            if (Number.isNaN(idx)) {
-                break;
-            }
-
-            let showDesc = false;
-            if (idx < 0) {
-                showDesc = true;
-                idx *= -1;
-            }
-
-            const entry = events[idx-1];
-            if (entry === undefined) {
-                break
-            }
-
-            if (showDesc) {
-                console.log(entry.eventDescription);
-                const _ = await this._question(enterToContinue);
-            } else {
-                await removeFcn(idx-1);
-                console.log("Event removed");
-                const _ = await this._question(enterToContinue);
-                break;
-            }
-        }
-    }
-
-    private async reinitializeMenu() {
-        await this._randomizerApp.reinitializeSettings();
-
-        console.log("Settings Reset");
-
-        const _ = await this._question(enterToContinue);
     }
 }
