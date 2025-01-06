6bb7e75a5a7d5ef579a6cc69682e1da4eaa578a7
brajac709 <bij4@cornell.edu>
2025-01-05 22:02:52 -0500

    Update page to load from test function

--- src/DateNightRandomizerWeb2/api/hello.ts
+++ src/DateNightRandomizerWeb2/api/hello.ts
@@ -1,9 +1,7 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function (request: VercelRequest, response: VercelResponse) {
-  response.status(200).send(/*{
+  response.status(200).send({
     message: `Hello from ${process.env.VERCEL_REGION}`
-  }*/ 
- `Hello from ${process.env.VERCEL_REGION}`
-    );
+    });
 };
